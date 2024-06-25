c2e199dbf59a9e002e9123d466c20ad16f34cc07
caio <caio@datahouseops.com>
2024-06-25 17:51:00 +0200

    Organize project folder

--- src/rlib_UIcommons.cpp
+++ src/rlib_UIcommons.cpp
@@ -2,30 +2,25 @@
 internal 
 void TextInputBox(Rectangle rectangle, bool *Active, u8 *CurrentCursorSprite, char* name, u32 *letterCount, u32 maxLenght)
 {
-    if (CheckCollisionPointRec(GetMousePosition(), rectangle))
+    if (*Active) *CurrentCursorSprite = MOUSE_CURSOR_IBEAM;
+    int key = GetCharPressed();
+    while (key > 0)
     {
-        if (*Active) *CurrentCursorSprite = MOUSE_CURSOR_IBEAM;
-        int key = GetCharPressed();
-        while (key > 0)
+        // NOTE: Only allow keys in range [32..125]
+        if ((key >= 32) && (key <= 125) && (*letterCount < maxLenght))
         {
-            // NOTE: Only allow keys in range [32..125]
-            if ((key >= 32) && (key <= 125) && (*letterCount < maxLenght))
-            {
-                name[*letterCount] = (char)key;
-                name[*letterCount+1] = '\0'; // Add null terminator at the end of the string.
-                (*letterCount)++;
-            }
-
-            key = GetCharPressed();  // Check next character in the queue
+            name[*letterCount] = (char)key;
+            name[*letterCount+1] = '\0'; // Add null terminator at the end of the string.
+            (*letterCount)++;
         }
 
-        if (IsKeyPressed(KEY_BACKSPACE))
-        {
-            (*letterCount)--;
-            if (*letterCount < 0) (*letterCount) = 0;
-            name[*letterCount] = '\0';
-        }
+        key = GetCharPressed();  // Check next character in the queue
+    }
 
+    if (IsKeyPressed(KEY_BACKSPACE))
+    {   
+        if (*letterCount > 0) (*letterCount)--;
+        name[*letterCount] = '\0';
     }
     DrawRectangleRec(rectangle, BLUE);
     // GuiDrawText(name, GetTextBounds(TOGGLE, rectangle), GuiGetStyle(TOGGLE, TEXT_ALIGNMENT), GetColor(GuiGetStyle(TOGGLE, TEXT + STATE_NORMAL*3)));
--- src/rlib_new_project_page.cpp
+++ src/rlib_new_project_page.cpp
@@ -1,8 +1,52 @@
+enum new_project_text_field
+{
+    ProjectNameField,
+    TagsField
+};
+
 u32 CreateProjectFolder(char *ProjectName)
 {   
-    char *PathStrs[] = {(char *)"..",(char *)"projects",ProjectName};
-    const char *FullPath = TextJoin((const char **)PathStrs,3,"/");
-    printf("Full path %s:\n", FullPath);
+    char *PathStrs[4];
+    const char *FullPath;
+
+    // 
+    //// Root Folder
+    // 
+    
+    PathStrs[0] = (char *)"..";
+    PathStrs[1] = (char *)"projects";
+    PathStrs[2] = ProjectName;
+    FullPath = TextJoin((const char **)PathStrs,3,"/");
+
+    if (!DirectoryExists(FullPath))
+    {
+        CreateDirectory(FullPath);
+    }; 
+
+    // 
+    //// Annotations Folder
+    // 
+
+    PathStrs[0] = (char *)"..";
+    PathStrs[1] = (char *)"projects";
+    PathStrs[2] = ProjectName;
+    PathStrs[3] = (char *)"annotations";
+    FullPath = TextJoin((const char **)PathStrs,4,"/");
+
+    if (!DirectoryExists(FullPath))
+    {
+        CreateDirectory(FullPath);
+    }; 
+
+    // 
+    //// Labels Folder
+    // 
+
+    PathStrs[0] = (char *)"..";
+    PathStrs[1] = (char *)"projects";
+    PathStrs[2] = ProjectName;
+    PathStrs[3] = (char *)"labels";
+    FullPath = TextJoin((const char **)PathStrs,4,"/");
 
     if (!DirectoryExists(FullPath))
     {
@@ -15,26 +59,53 @@ u32 CreateProjectFolder(char *ProjectName)
 internal
 u32 NewProjectPage()
 {
+    u32 CurrentGesture = GetGestureDetected();
     u32 ScreenWidth = GetScreenWidth();
+    u32 ScreenHeight = GetScreenHeight();
     u32 FontSize = (u32)ScreenWidth*0.02;
     u32 ProjectNameW = MeasureText("Project Name:",FontSize);
+
     Rectangle ProjectNameRec = {.x = ScreenWidth/2.0f + 10, .y = 100, .width = 300, .height = 30};
-    internal bool Active = true;
-    internal u32 LetterCount = 0;
+    Rectangle TagsRec = {.x = ScreenWidth/2.0f + 10, .y = 100 + 35, .width = 300, .height = 30};
+    Rectangle DoneButton = {.x = ScreenWidth*0.8f, .y = ScreenHeight*0.85f, .width = ScreenWidth*0.1f, .height = ScreenHeight*0.05f};
+
+    internal bool ProjectNameActive = false;
+    internal bool TagsActive = false;
+    internal u32 ProjectNameLetterCount = 0;
+    internal u32 TagsLetterCount = 0;
+
+    char Tags[16]= {0};
     
     BeginDrawing();
         ClearBackground(GREEN);
         DrawText("Project Name:", ScreenWidth/2 - ProjectNameW,100,FontSize,BLACK);
-        TextInputBox(ProjectNameRec, &Active, &CurrentCursorSprite, ProjectName, &LetterCount, MAX_LENGTH);
-        DrawText(ProjectName, (u32)ProjectNameRec.x + 5, (u32)ProjectNameRec.y + 5, 20, BLACK);
-        if (IsKeyPressed(KEY_ENTER))
+        DrawText("Tags:", ScreenWidth/2 - ProjectNameW,100 + 35,FontSize,BLACK);
+        TextInputBox(ProjectNameRec, &ProjectNameActive, &CurrentCursorSprite, ProjectName, &ProjectNameLetterCount, MAX_LENGTH);
+        TextInputBox(TagsRec, &TagsActive, &CurrentCursorSprite, Tags, &TagsLetterCount, MAX_LENGTH);
+
+        if (CheckCollisionPointRec(GetMousePosition(), ProjectNameRec) && (IsGestureTapped(CurrentGesture)))
         {
-            Active = false;
-            IsProjectNameSet = true;
-            printf("Project name: %s\n", ProjectName);
-            CreateProjectFolder(ProjectName);
+            ProjectNameActive = true;
         } 
+
+        DrawText(ProjectName, (u32)ProjectNameRec.x + 5, (u32)ProjectNameRec.y + 5, 20, BLACK);
+        if (IsKeyPressed(KEY_ENTER) && ProjectNameActive)
+        {
+            IsProjectNameSet = true;  
+            ProjectNameActive = false;
+            TagsActive = true;
+        }
+        else if (IsKeyPressed(KEY_ENTER) && TagsActive)
+        {
+            TagsActive = false;
+        };
+
         if(GuiButton({PANELWIDTH*0.6,10,PANELWIDTH*0.19,30},"BACK")) return FRONT_PAGE;
+        if(GuiButton(DoneButton,"Done!"))
+        {
+            CreateProjectFolder(ProjectName);
+            return FRONT_PAGE;
+        } 
         DrawFPS(10,10);
     EndDrawing();
 
